714ca5b41a4934b0136cca21bbad429122e5dae6
SKhrebtan <shrebtan@gmail.com>
2023-07-04 22:10:34 +0300

    add swipe

---
+++ src/js/swipe.js
@@ -0,0 +1,27 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+    loop: true,
+     modules: [Navigation, Pagination],
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
